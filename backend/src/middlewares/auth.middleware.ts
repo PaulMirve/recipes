981e13f4e2af73ac098ed88a5b56676de3401921
Paul Miranda <paul_mv3@hotmail.com>
2021-10-27 09:50:24 -0700

    The auth middleware atach user to the context

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,13 +1,22 @@
 import { MiddlewareFn } from "type-graphql";
+import ValidateJWT from "../helpers/validate-jwt";
+import { UserEntity } from "../schema/user/user.entity";
 
 interface IContext {
     headers: {
         authorization: string
-    }
+    },
+    user: UserEntity
 }
-export const AuthMiddleware: MiddlewareFn<IContext> = ({ context }, next) => {
-    if(!context.headers.authorization){
+export const AuthMiddleware: MiddlewareFn<IContext> = async ({ context }, next) => {
+    if (!context.headers.authorization) {
         throw new Error('Unauthorized');
     }
+    const { username } = ValidateJWT(context.headers.authorization) as { username: string, iat: number, exp: number };
+    const user = await UserEntity.findOne({ username });
+    if (!user) {
+        throw new Error('Unauthorized');
+    }
+    context.user = user;
     return next();
 }
--- backend/src/schema/user/user.resolver.ts
+++ backend/src/schema/user/user.resolver.ts
@@ -1,12 +1,17 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { UserEntity } from "./user.entity";
 import { User, UserInput } from "./user.types";
 import bcrypt from 'bcrypt';
+import { AuthMiddleware } from "../../middlewares/auth.middleware";
+import { Context } from "apollo-server-core";
 
 @Resolver()
 export class UserResolver {
     @Query(returns => [User])
-    getUsers() {
+    @UseMiddleware(AuthMiddleware)
+    getUsers(
+        @Ctx("user") user: UserEntity
+    ) {
         return UserEntity.find();
     }
 
