832a50f98e39235fd3035d73787229e82d610661
Paul Miranda <paul_mv3@hotmail.com>
2021-11-20 00:08:09 -0800

    validated photos extensions

--- backend/src/helpers/upload-photo.ts
+++ backend/src/helpers/upload-photo.ts
@@ -4,6 +4,13 @@ import { v2 as cloudinary } from 'cloudinary'
 
 export const uploadPhoto = async (photo: FileUpload): Promise<string> => {
     const { filename, createReadStream } = await photo;
+    const nameSplitted = filename.split('.');
+    const allowedExtensions = ['png', 'jpg', 'jpeg', 'gif'];
+    const extension = nameSplitted[nameSplitted.length - 1];
+    if(!allowedExtensions.includes(extension)) {
+        throw new Error("This extension isn't allowed");
+    }
+
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
