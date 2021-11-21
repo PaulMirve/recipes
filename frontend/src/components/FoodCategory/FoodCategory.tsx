a25fdb074e572c8cf0020f541bdbb1d6da26d65e
Paul Miranda <paul_mv3@hotmail.com>
2021-11-21 00:59:50 -0800

    added sections to the landing

--- frontend/src/components/FoodCategory/FoodCategory.tsx
+++ frontend/src/components/FoodCategory/FoodCategory.tsx
@@ -1,13 +1,16 @@
 import { Icon } from '../Icon'
 
+export type Countries = "mexico" | "india" | "japan" | "greece" | "china" | "italy"
+
 interface Props {
-
+    icon: Countries,
+    name: string
 }
-
-export const FoodCategory = (props: Props) => {
+export const FoodCategory = ({ icon, name }: Props) => {
     return (
         <div className="food-category">
-            <Icon icon="china" />
+            <Icon icon={icon} />
+            <div className="food-category__name">{name}</div>
         </div>
     )
 }
