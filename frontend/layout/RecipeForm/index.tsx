4d77290bee35c29f55143893f27d396d5103c0c3
Paul Miranda <paul_mv3@hotmail.com>
2021-11-30 08:32:19 -0800

    extracted the recipes form submit function as a prop

--- frontend/layout/RecipeForm/index.tsx
+++ frontend/layout/RecipeForm/index.tsx
@@ -1,5 +1,4 @@
 import styles from '@sass/pages/add-recipe.module.scss'
-import axios from 'axios'
 import { FloatingButton } from 'components/FloatingButton'
 import Heading from 'components/Heading'
 import Icon from 'components/Icon'
@@ -8,24 +7,33 @@ import { FormikTextArea } from 'components/TextArea/FormikTextArea'
 import { FormikTextInput } from 'components/TextInput'
 import { Form, Formik } from 'formik'
 import { Ingredient, Step, useGetUnitsQuery } from 'generated/graphql'
-import { useRouter } from 'next/dist/client/router'
 import { useState } from 'react'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
 import * as Yup from 'yup'
 import { IngredientsForm } from './IngredientsForm'
 import { StepsForms } from './StepsForms'
 
-const MySwal = withReactContent(Swal);
+
+
+export interface RecipeFormSubmitArgs {
+    name: string,
+    people: number,
+    description: string,
+    photo: string | undefined,
+    photoFile: Blob | undefined,
+    steps: Step[],
+    ingredients: Ingredient[],
+    tags: string[]
+}
 
 interface Props {
     tags?: string[],
     ingredients?: Ingredient[],
-    steps?: Step[]
+    steps?: Step[],
+    onFormSubmit: (args: RecipeFormSubmitArgs) => void
 }
 
-const RecipeForm = ({ tags: _tags = [], ingredients: _ingredients = [], steps: _steps = [] }: Props) => {
-    const router = useRouter();
+const RecipeForm = ({ tags: _tags = [], ingredients: _ingredients = [], steps: _steps = [], onFormSubmit }: Props) => {
+
     const [photo, setPhoto] = useState<string | undefined>();
     const [photoFile, setPhotoFile] = useState<Blob>()
     const [tags, setTags] = useState<string[]>(_tags)
@@ -44,105 +52,6 @@ const RecipeForm = ({ tags: _tags = [], ingredients: _ingredients = [], steps: _
         }
     }
 
-    const onFormSubmit = async ({ name, people, description }: {
-        name: string;
-        people: number;
-        description: string;
-    }) => {
-        Swal.fire({
-            title: 'Loading...',
-            customClass: {
-                popup: styles.alert
-            },
-            allowOutsideClick: false
-        });
-        Swal.showLoading();
-        const errors = [];
-        if (!photo) {
-            errors.push('The recipe needs a photo.');
-        }
-        if (steps.length === 0) {
-            errors.push('The recipe needs one or more steps.');
-        }
-        if (ingredients.length === 0) {
-            errors.push('The recipe needs one or more ingredients.');
-        }
-        if (tags.length === 0) {
-            errors.push('The recipe needs one or more tags.');
-        }
-
-        if (errors.length === 0) {
-            let formData = new FormData();
-            formData.append("operations", `
-            {
-                "query": "mutation ($recipe: RecipeInput!) {saveRecipe(recipe: $recipe){name} }",
-                "variables": {
-                    "recipe": {
-                        "name": "${name}",
-                        "description": "${description}",
-                        "photo": null,
-                        "numberOfPeople": ${people},
-                        "ingredients": [${ingredients.map(({ name, quantity, unit: { idUnit } }) => JSON.stringify({ name, quantity: Number(quantity), idUnit: Number(idUnit) }))}],
-                        "steps": [${steps.map(({ description }) => JSON.stringify({ description }))}],
-                        "tags": [${tags.map((tag) => JSON.stringify({ name: tag, idRecipe: 0 }))}]
-                    }
-                }
-            }
-            `);
-            formData.append("map", `{ "0": ["variables.recipe.photo"] }`);
-            formData.append("0", photoFile!);
-            try {
-                await axios.post('http://localhost:8081/graphql', formData, {
-                    headers: {
-                        authorization: localStorage.getItem('token') || ""
-                    }
-                });
-                Swal.hideLoading();
-                MySwal.fire({
-                    title: 'Recipe added successfully!',
-                    text: 'The recipe has been added successfully, please press continue to see the recipes.',
-                    icon: 'success',
-                    allowOutsideClick: false,
-                    customClass: {
-                        popup: styles.alert
-                    },
-                    confirmButtonText: 'Continue'
-                }).then(result => {
-                    if (result.isConfirmed) {
-                        router.push('/recipes');
-                    }
-                })
-            } catch (err) {
-                Swal.hideLoading();
-                alert(err);
-            }
-
-        } else {
-            Swal.hideLoading();
-            MySwal.fire({
-                title: 'Recipe incomplete!',
-                html: (
-                    <span>
-                        <h4 style={{ marginBottom: '1rem' }}>The information of the recipe is incomplete:</h4>
-                        <ul>
-                            {
-                                errors.map(error => (
-                                    <li className={styles.errors}>{error}</li>
-                                ))
-                            }
-                        </ul>
-                    </span>
-                ),
-                icon: 'error',
-                allowOutsideClick: false,
-                customClass: {
-                    popup: styles.alert
-                },
-                confirmButtonText: 'Accept'
-            })
-        }
-    }
-
     return (
         <div className={styles.main}>
             <Heading variant="h1" fontWeight='bold'>Add new recipe</Heading>
@@ -157,7 +66,7 @@ const RecipeForm = ({ tags: _tags = [], ingredients: _ingredients = [], steps: _
                     people: Yup.number().min(1, 'The recipe can not be for less than one people'),
                     description: Yup.string().required('The description of the recipe is required')
                 })}
-                onSubmit={onFormSubmit}>
+                onSubmit={({ name, description, people }) => onFormSubmit({ name, people, description, photo, photoFile, steps, ingredients, tags })}>
                 {
                     formik => (
                         <Form>
--- frontend/pages/recipes/add.tsx
+++ frontend/pages/recipes/add.tsx
@@ -1,9 +1,111 @@
-import RecipeForm from 'layout/RecipeForm'
-
+const MySwal = withReactContent(Swal);
+import styles from '@sass/pages/add-recipe.module.scss';
+import axios from 'axios';
+import RecipeForm, { RecipeFormSubmitArgs } from 'layout/RecipeForm';
+import { useRouter } from 'next/dist/client/router';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 const AddRecipe = () => {
+    const router = useRouter();
+
+    const onFormSubmit = async ({ name, description, people, photo, photoFile, steps, ingredients, tags }: RecipeFormSubmitArgs) => {
+        Swal.fire({
+            title: 'Loading...',
+            customClass: {
+                popup: styles.alert
+            },
+            allowOutsideClick: false
+        });
+        Swal.showLoading();
+        const errors = [];
+        if (!photo) {
+            errors.push('The recipe needs a photo.');
+        }
+        if (steps.length === 0) {
+            errors.push('The recipe needs one or more steps.');
+        }
+        if (ingredients.length === 0) {
+            errors.push('The recipe needs one or more ingredients.');
+        }
+        if (tags.length === 0) {
+            errors.push('The recipe needs one or more tags.');
+        }
+
+        if (errors.length === 0) {
+            let formData = new FormData();
+            formData.append("operations", `
+            {
+                "query": "mutation ($recipe: RecipeInput!) {saveRecipe(recipe: $recipe){name} }",
+                "variables": {
+                    "recipe": {
+                        "name": "${name}",
+                        "description": "${description}",
+                        "photo": null,
+                        "numberOfPeople": ${people},
+                        "ingredients": [${ingredients.map(({ name, quantity, unit: { idUnit } }) => JSON.stringify({ name, quantity: Number(quantity), idUnit: Number(idUnit) }))}],
+                        "steps": [${steps.map(({ description }) => JSON.stringify({ description }))}],
+                        "tags": [${tags.map((tag) => JSON.stringify({ name: tag, idRecipe: 0 }))}]
+                    }
+                }
+            }
+            `);
+            formData.append("map", `{ "0": ["variables.recipe.photo"] }`);
+            formData.append("0", photoFile!);
+            try {
+                await axios.post('http://localhost:8081/graphql', formData, {
+                    headers: {
+                        authorization: localStorage.getItem('token') || ""
+                    }
+                });
+                Swal.hideLoading();
+                MySwal.fire({
+                    title: 'Recipe added successfully!',
+                    text: 'The recipe has been added successfully, please press continue to see the recipes.',
+                    icon: 'success',
+                    allowOutsideClick: false,
+                    customClass: {
+                        popup: styles.alert
+                    },
+                    confirmButtonText: 'Continue'
+                }).then(result => {
+                    if (result.isConfirmed) {
+                        router.push('/recipes');
+                    }
+                })
+            } catch (err) {
+                Swal.hideLoading();
+                alert(err);
+            }
+
+        } else {
+            Swal.hideLoading();
+            MySwal.fire({
+                title: 'Recipe incomplete!',
+                html: (
+                    <span>
+                        <h4 style={{ marginBottom: '1rem' }}>The information of the recipe is incomplete:</h4>
+                        <ul>
+                            {
+                                errors.map(error => (
+                                    <li className={styles.errors}>{error}</li>
+                                ))
+                            }
+                        </ul>
+                    </span>
+                ),
+                icon: 'error',
+                allowOutsideClick: false,
+                customClass: {
+                    popup: styles.alert
+                },
+                confirmButtonText: 'Accept'
+            })
+        }
+    }
+
     return (
-        <RecipeForm />
+        <RecipeForm onFormSubmit={onFormSubmit} />
     )
 }
 
