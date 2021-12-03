ba1f135b9322beac6f9368b91fbca6fcd1579a75
Paul Miranda <paul_mv3@hotmail.com>
2021-12-03 15:19:43 -0800

    added animation to the menu and fixed the position of the menu when the screen resizes

--- frontend/components/Menu/ìndex.tsx
+++ frontend/components/Menu/ìndex.tsx
@@ -9,17 +9,31 @@ interface Props {
 
 const Menu = ({ anchorEl, open, onClose, children, className = "", ...props }: Props & ComponentPropsWithoutRef<'div'>) => {
     const menu = useRef<HTMLDivElement | null>(null)
-    useEffect(() => {
+
+    const positionMenu = () => {
         if (anchorEl && menu.current) {
             const { top, left, height, width } = anchorEl.getBoundingClientRect();
             menu.current.style.top = `calc(${top}px + ${height}px + 5px)`;
-            menu.current.style.left = `calc(${left}px - ${width}px)`;
+            menu.current.style.left = `calc(${left}px - ${width}px - 20px)`;
         }
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", positionMenu, false);
+        positionMenu();
     }, [anchorEl])
 
+    const onMenuClose = () => {
+        menu.current && menu.current.classList.add(styles.onClose);
+        setTimeout(() => {
+            onClose();
+            menu.current && menu.current.classList.remove(styles.onClose);
+        }, 200);
+    }
+
     return (
-        <div className={`${styles.wrapper} ${open && styles.open}`} onClick={onClose}>
-            <div ref={menu} className={styles.menu} onClick={e => e.stopPropagation()} {...props}>
+        <div className={`${styles.wrapper} ${open && styles.open}`} onClick={onMenuClose}>
+            <div ref={menu} className={`${styles.menu} ${open && styles.menuOpen}`} onClick={e => e.stopPropagation()} {...props}>
                 {children}
             </div>
         </div>
--- frontend/components/MobileMenu/index.tsx
+++ frontend/components/MobileMenu/index.tsx
@@ -11,19 +11,19 @@ interface Props {
 
 const MobileMenu = ({ onClose, open }: Props) => {
     const { user } = useGlobalContext();
-    const wrapper = useRef<HTMLDivElement>(null);
+    const menu = useRef<HTMLDivElement>(null);
 
     const closeMenu = () => {
-        wrapper.current?.classList.add(styles.onClose);
+        menu.current?.classList.add(styles.onClose);
         setTimeout(() => {
             onClose();
-            wrapper.current?.classList.remove(styles.onClose);
+            menu.current?.classList.remove(styles.onClose);
         }, 500)
     }
 
     return (
         <div className={`${styles.wrapper} ${open ? styles.open : styles.close}`} onClick={closeMenu}>
-            <nav ref={wrapper} className={styles.main} onClick={e => e.stopPropagation()}>
+            <nav ref={menu} className={styles.main} onClick={e => e.stopPropagation()}>
                 {
                     user ? <LoggedMenu user={user} /> : <NotLoggedMenu />
                 }
