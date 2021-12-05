fdf7b72998e4ac0ae8cd0240c05d3f17f034f566
Paul Miranda <paul_mv3@hotmail.com>
2021-12-04 18:20:36 -0800

    users now can be searched by name and lastname and recipes by tags and description

--- backend/src/schema/search/search.resolver.ts
+++ backend/src/schema/search/search.resolver.ts
@@ -10,11 +10,15 @@ export class SearchResolver {
         const users = await UserEntity
             .createQueryBuilder("user")
             .where("user.username ilike :username", { username: `%${phrase}%` })
+            .orWhere("user.name ilike :name", { name: `%${phrase}%` })
+            .orWhere("user.lastName ilike :lastName", { lastName: `%${phrase}%` })
             .getMany();
         const recipes = await RecipeEntity
             .createQueryBuilder("recipe")
+            .innerJoinAndSelect("recipe.tags", "tags")
             .where("recipe.name ilike :name", { name: `%${phrase}%` })
             .orWhere("recipe.description ilike :description", { description: `%${phrase}%` })
+            .orWhere("tags.name ilike :name", { name: `%${phrase}%` })
             .getMany();
         return [...users, ...recipes];
     }
